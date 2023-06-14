1cc8fc6129b1370f7e0b586eacce1cec2343a5c4
uadarksamurai <itandeducationua@gmail.com>
2023-06-14 14:25:44 +0300

    profile 1.0

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import { Profile } from "./Profile/Profile";
+import user from "./Profile/user"
+export const App = () => {
+  return (
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+    </div>
+  );
+};
