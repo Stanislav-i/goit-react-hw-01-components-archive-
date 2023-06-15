caef26be9f1efa7e7a7d80da3e21e3285b2c9341
uadarksamurai <itandeducationua@gmail.com>
2023-06-15 15:05:38 +0300

    profile 1.2

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -6,9 +6,9 @@ export const Profile = ({ username, tag, location, avatar, stats }) => {
     <div className={css.profile}>
       <div className={css.description}>
         <img src={avatar} alt="User avatar" className={css.avatar} />
-        <p className="name">{username}</p>
-        <p className="tag">@{tag}</p>
-        <p className="location">{location}</p>
+        <p className={css.name}>{username}</p>
+        <p className={css.tag}>@{tag}</p>
+        <p className={css.location}>{location}</p>
       </div>
 
       <ul className="stats">
